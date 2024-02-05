d20d8744cfceded3b49bd9649d9bc361e0905ebe
Roel Vanhout <roel.vanhout@gmail.com>
2024-02-05 16:05:22 +0100

    Include entire build cyle, build both release and debug

--- build_vcpkg.bat
+++ build_vcpkg.bat
@@ -1,11 +1,9 @@
-rem vcpkg install
+vcpkg install
 
 mkdir build
 
-
-rem --trace ^
 cmake -B build -S . ^
-    -DCMAKE_TOOLCHAIN_FILE=d:/vcpkg/scripts/buildsystems/vcpkg.cmake ^
-    -DCMAKE_PREFIX_PATH=e:\Dropbox\bin\win_flex_bison-2.5.25
+    -DCMAKE_TOOLCHAIN_FILE=d:/vcpkg/scripts/buildsystems/vcpkg.cmake
 
-rem cmake --build build
+cmake --build build --config Debug
+cmake --build build --config Release
